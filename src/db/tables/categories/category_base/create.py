f6a825a680d91c1f233db8a9a893d1522ae4793d
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-12-11 23:10:47 -0600

    fixed action type create bug

--- src/db/tables/categories/category_base/create.py
+++ src/db/tables/categories/category_base/create.py
@@ -7,8 +7,8 @@ def create(self: Category) -> int:
     self.__logger__.info(f"Creating new {self.__class__}: {self}")
     core = DbCore()
     core.logger = self.__logger__
-    query = f"INSERT INTO {self.__table_name__} (name, description) VALUES (?, ?)"
-    params = (self.name, self.description)
+    query = f"INSERT INTO {self.__table_name__} (name, description, color) VALUES (?, ?, ?)"
+    params = (self.name, self.description, self.color)
     exception_package = ExceptionPackage(
         unique_constraint_error=f"{self.__class__} name '{self.name}' already exists"
     )
