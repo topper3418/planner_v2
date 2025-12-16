0f113e8a255223dd7dc72ced2f418b2eae20a528
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-12-15 23:21:30 -0600

    after further refactor, still works

--- src/db/core/__init__.py
+++ src/db/core/__init__.py
@@ -2,7 +2,7 @@
 
 from .get_connection import get_db_connection
 
-from .sql_builder import QueryBuilder, InsertBuilder
+from .sql_builder import QueryBuilder, get_update_query, get_insert_query
 from .table_model import TableModel
 from .query_params import QueryParams
 from .fields import (
